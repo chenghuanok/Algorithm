f8d52e3546b2cc09c4be4736fca76c406f40ac8c
chenghuan <1832398432@qq.com>
2019-10-26 23:21:50 +0800

    二叉查找树删除

--- src/cn/struct/algorithm/tree/search/BSTree.java
+++ src/cn/struct/algorithm/tree/search/BSTree.java
@@ -132,5 +132,105 @@ public class BSTree <T extends Comparable<T>>{
         BSTNode<T> insertNode = new BSTNode<>(data,null,null,null);
         insert(this,insertNode);
     }
+
+    /**
+     * 查找最小结点：返回rootNodeTree为根结点的二叉树的最小结点。
+     * @param rootNodeTree
+     * @return BSTNode
+     * @author chenghuan
+     * @date 2019/10/26 21:32
+     */
+    private BSTNode<T> minNode(BSTNode<T> rootNodeTree){
+        if(rootNodeTree==null){
+            return null;
+        }
+        BSTNode<T> minNode = rootNodeTree;
+        while(minNode.left!=null){
+            minNode = minNode.left;
+        }
+        return minNode;
+    }
+
+    /**
+     * 找结点node的后继结点。即，查找"二叉树中数据值大于该结点"的"最小结点"。
+     * @param node
+     * @return BSTNode
+     * @author chenghuan
+     *  2019/10/26 22:06
+     */
+    private BSTNode<T> successor(BSTNode<T> node){
+        if(node.right!=null){
+            return minNode(node.right);
+        }
+        BSTNode<T> targetNode = node.parent;
+        //1.node是一个左节点，则node的后继节点为他的父节点
+        //2.node是一个右节点，则node的后继节点为他的父节点的父节点
+        if(targetNode!=null&&node == targetNode.right){
+            targetNode = targetNode.parent;
+        }
+        return targetNode;
+    }
+
+    /**
+     * 删除指定的节点
+     * @param node
+     * @author chenghuan
+     * @date 2019/10/26 22:17
+     */
+    private void delete(BSTNode<T> node){
+        if(node.right!=null&&node.left!=null){
+            //找到该节点的后继节点
+            BSTNode<T> successorNode = successor(node);
+            //讲后继节点的值设置到当前节点
+            node.data = successorNode.data;
+            //讲删除的当前节点设置为后继节点
+            node = successorNode;
+        }
+        BSTNode<T> child ;
+        if(node.left!=null){
+            child = node.left;
+        }else{
+            child = node.right;
+        }
+        //如果child不为null,则node有一个节点
+        if(child!=null){
+           child.parent = node.parent;
+        }
+        //当前节点没有父节点，要删除的是根节点
+        if(node.parent==null){
+            mRoot = child;
+        }else if(node == node.parent.left){
+            node.parent.left = child;
+        }else if(node == node.parent.right){
+            node.parent.right = child;
+        }
+    }
+
+    /**
+     * 删除
+     * @param data
+     * @author chenghuan
+     * @date 2019/10/26 23:06
+     */
+    public void delete(T data){
+         //查找要删除的节点
+        BSTNode<T> node = search(data);
+        if(node!=null){
+            delete(node);
+        }
+    }
+
+    /**
+     * 中序遍历
+     *@author chenghuan
+     *@date 2019/10/26 22:55
+     */
+    public void middle(BSTNode<T> tree){
+      if(tree!=null){
+          middle(tree.left);
+          System.out.print(tree.data+",");
+          middle(tree.right);
+      }
+    }
 }
 
---
+++ src/cn/struct/algorithm/tree/search/BSTreeTest.java
@@ -0,0 +1,49 @@
+package cn.struct.algorithm.tree.search;
+
+import cn.struct.algorithm.tree.traverse.TreeTraverse;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+
+/**
+ * 二叉查找树测试
+ * @Author: chenghuan
+ * @Date: 2019/10/26 22:47
+ */
+public class BSTreeTest {
+
+    @Test
+    public void insert(){
+        final BSTree<Integer> bsTree = new BSTree<>();
+        IntStream.range(1,6).forEach(i -> {
+            bsTree.insert(i);
+        });
+        //打印
+        bsTree.middle(bsTree.mRoot);
+    }
+
+    @Test
+    public void search(){
+        final BSTree<Integer> bsTree = new BSTree<>();
+        IntStream.range(1,6).forEach(i -> {
+            bsTree.insert(i);
+        });
+        final BSTree.BSTNode node = bsTree.search(6);
+        if(node!=null){
+            System.out.println("查找的值存在！！！");
+        }else{
+            System.out.println("查找的值不存在！！！");
+        }
+    }
+
+    @Test
+    public void delete(){
+        final BSTree<Integer> bsTree = new BSTree<>();
+        IntStream.range(1,6).forEach(i -> {
+            bsTree.insert(i);
+        });
+        bsTree.delete(1);
+        //打印
+        bsTree.middle(bsTree.mRoot);
+    }
+}

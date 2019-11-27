af96de24421b3b657631130fa5a4d8501751a707
chenghuan <1832398432@qq.com>
2019-11-27 22:23:18 +0800

    二分查找变形

--- src/cn/struct/algorithm/binary/search/Search.java
+++ src/cn/struct/algorithm/binary/search/Search.java
@@ -8,6 +8,7 @@ package cn.struct.algorithm.binary.search;
 public class Search {
 
     /**
+     * 查找值等于给定值(不存在重复元素)
      * @param a, value
      * @return boolean
      * @author chenghuan
@@ -27,5 +28,82 @@ public class Search {
             }
         }
         return false;
+    }
+
+    /**
+     * 查找第一个值等于给定值的位置
+     * @param a value
+     * @return int
+     * @author chenghuan
+     * @date 2019/11/27 21:54
+    */
+    public int findFirstEqualValue(int[] a, int value){
+         int low = 0;
+         int high = a.length-1;
+         while(low<=high){
+             int middle = (low+high)/2;
+             if(a[middle]>value){
+                 high = middle - 1;
+             }else if(a[middle]<value){
+                 low = middle+1;
+             }else{
+                 if(middle ==0 ||a[middle-1] != value){
+                     return middle;
+                 }
+                 high = middle -1;
+             }
+         }
+         return -1;
+    }
+
+    /**
+     * 查找最后一个值等于给定值得位置
+     * @param a value
+     * @return int
+     * @author chenghuan
+     * @date 2019/11/27 22:12
+    */
+    public int findEndEqualValue(int[] a, int value){
+        int low = 0;
+        int high = a.length-1;
+        while(low<=high){
+            int middle = (low+high)/2;
+            if(a[middle]>value){
+                high = middle - 1;
+            }else if(a[middle]<value){
+                low = middle+1;
+            }else{
+                if(middle ==a.length-1 ||a[middle+1] != value){
+                    return middle;
+                }
+                low = middle +1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 查找第一个大于等于给定值的位置
+     * @param  a value
+     * @return  int
+     * @author chenghuan
+     * @date 2019/11/27 22:17
+    */
+    public int findFirstThanValue(int[] a, int value){
+        int low = 0;
+        int high = a.length-1;
+        while (low<=high){
+            int middle = (low+high)/2;
+            if(a[middle]>=value){
+                if(middle == 0||a[middle-1]<value){
+                    return middle;
+                }else{
+                    high = middle -1;
+                }
+            }else {
+                low = middle+1;
+            }
+        }
+        return  -1;
     }
 }
--- src/cn/struct/algorithm/binary/search/SearchTest.java
+++ src/cn/struct/algorithm/binary/search/SearchTest.java
@@ -15,4 +15,28 @@ public class SearchTest {
       final boolean existFlag = search.binarySearch(a,7);
       System.out.println(existFlag);
     }
+
+    @Test
+    public void findFirstEqualValue(){
+        final Search search = new Search();
+        int[] a ={1,3,4,5,6,8,8,8,9,10};
+        final int position = search.findFirstEqualValue(a,8);
+        System.out.println(position);
+    }
+
+    @Test
+    public void findEndEqualValue(){
+        final Search search = new Search();
+        int[] a ={1,3,4,5,6,8,8,8,9,10};
+        final int position = search.findEndEqualValue(a,8);
+        System.out.println(position);
+    }
+
+    @Test
+    public void findFirstThanValue(){
+        final Search search = new Search();
+        int[] a ={3,4,6,7,10};
+        final int position = search.findFirstThanValue(a,9);
+        System.out.println(position);
+    }
 }
